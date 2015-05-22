a4302b3d87dbb6e4fc2460f2b80e760913983d25
unknown <vazquez@DA27063.in.dynetics.com>
2015-05-22 08:29:27 -0500

    Adding code to repo

---
+++ Fruit.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryingFruit
+{
+    enum PowerLevel { ROOKIE = 1, SUPERNOVA, CAPTAIN=SUPERNOVA, ADMIRAL, DIVISION_COMMANDER=ADMIRAL, YONKO, FLEET_ADMIRAL=YONKO}
+
+    class Fruit
+    {
+        private const int DEFAULT_DIAM = 5;
+
+        private string name;
+        private int diameter;
+        private string power;
+        private PowerLevel powerLevel;
+
+        public Fruit(string name, int diameter)
+        {
+            this.name = name;
+            this.diameter = diameter;
+            this.power = "none";
+            this.powerLevel = 0;
+        }
+
+        public Fruit(string name, string power, PowerLevel powerLevel)
+        {
+            this.name = name;
+            this.diameter = DEFAULT_DIAM;
+            this.power = power;
+            this.powerLevel = powerLevel;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+        }
+
+        public int Diameter
+        {
+            get { return this.diameter; }
+        }
+
+        public string Power
+        {
+            get { return this.power; }
+        }
+
+        public PowerLevel PowerLevel
+        {
+            get { return this.powerLevel; }
+        }
+
+        public bool normalFruit()
+        {
+            if (this.powerLevel == 0)
+                return true;
+            return false;
+        }
+
+        public override string ToString()
+        {
+            string str = "";
+            if (name.Length >= 9)
+                str += "Fruit: " + name + "\tPower: ";
+            else
+                str += "Fruit: " + name + "\t\tPower: ";
+            if (this.power.Length >= 9)
+                str += power + "\tLevel: " + powerLevel;
+            else
+                str += power + "\t\tLevel: " + powerLevel;
+
+            return str;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueryingFruit
+{
+    class Program
+    {
+
+        static void newLine()
+        {
+            Console.WriteLine("\n");
+        }
+
+        static Fruit[] loadFruits()
+        {
+            var fruits = new[] { new Fruit("apple", 4),
+                                 new Fruit("grape", 1), 
+                                 new Fruit("kiwi", 2), 
+                                 new Fruit("watermelon",8),
+                                 new Fruit("Gum Gum", "Rubber-man", PowerLevel.SUPERNOVA),
+                                 new Fruit("Fire Fire", "Fire", PowerLevel.DIVISION_COMMANDER),
+                                 new Fruit("Quake Quake", "Earthquake", PowerLevel.YONKO),
+                                 new Fruit("Ice Ice", "Ice", PowerLevel.ADMIRAL),
+                                 new Fruit("strawberry", 2),
+                                 new Fruit("Magma Magma", "Magma", PowerLevel.ADMIRAL),
+                                 new Fruit("Smoke Smoke", "Smoke", PowerLevel.CAPTAIN),
+                                 new Fruit("watermelon",8),
+                                 new Fruit("banana", 2),
+                                 new Fruit("blueberry", 1)
+                               };
+            return fruits;
+        }
+
+        static void queryFruitByNameAscending(Fruit[] fruits)
+        {
+            IEnumerable<Fruit> query =
+                    from fruit in fruits
+                    orderby fruit.Name ascending
+                    select fruit;
+
+            foreach(var fruit in query)
+            {                
+                if (fruit.Name.Length >= 10)
+                    Console.WriteLine("Fruit: " + fruit.Name + "\tDiam: " + fruit.Diameter);
+                else
+                    Console.WriteLine("Fruit: " + fruit.Name + "\t\tDiam: " + fruit.Diameter);              
+            }
+        }
+
+        static void queryFruitByDiameterAscending(Fruit[] fruits)
+        {
+            IEnumerable<Fruit> query =
+                    from fruit in fruits
+                    orderby fruit.Diameter ascending
+                    select fruit;
+
+            foreach (var fruit in query)
+            {
+                if (fruit.Name.Length >= 10)
+                    Console.WriteLine("Fruit: " + fruit.Name + "\tDiam: " + fruit.Diameter);
+                else
+                    Console.WriteLine("Fruit: " + fruit.Name + "\t\tDiam: " + fruit.Diameter);
+            }
+        }
+
+        static void Main()
+        {           
+            /* Using Anonymous data types */
+            var austin = new { lastName = "Vazquez\n", color = "blue", number = 4 };
+            Console.WriteLine("\n\t\t\tAustin " + austin.lastName);
+
+            var fruits = loadFruits();
+
+            /* Querying by properties */
+            var DevilFruitsNames =
+                from fruit in fruits
+                where !fruit.normalFruit()
+                orderby fruit.PowerLevel ascending
+                select fruit.PowerLevel;
+
+            var NonDevilFruitsNames =
+                from fruit in fruits
+                where fruit.normalFruit() && fruit.Name.StartsWith("b")
+                orderby fruit.Name ascending
+                select fruit.Name;
+
+            foreach (var fruit in NonDevilFruitsNames)
+            {
+                Console.WriteLine(fruit);
+            }
+        }
+    }
+}
